9a2df569abdc7be07c710f2cfa28a1fcfc0fb895
Kevin Tun <kevin.tun@northwestern.edu>
2022-10-25 22:41:10 -0500

    fix: add nodes for formatting, links, and lists

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -5,11 +5,19 @@ import { Container, Paper } from "@material-ui/core"
 import { LexicalComposer } from "@lexical/react/LexicalComposer"
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
 import { ContentEditable } from "@lexical/react/LexicalContentEditable"
+import { LinkPlugin } from "@lexical/react/LexicalLinkPlugin"
+import { ListPlugin } from "@lexical/react/LexicalListPlugin"
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
+import { HeadingNode, QuoteNode } from "@lexical/rich-text"
+import { TableCellNode, TableNode, TableRowNode } from "@lexical/table"
+import { ListItemNode, ListNode } from "@lexical/list"
+import { CodeHighlightNode, CodeNode } from "@lexical/code"
+import { LinkNode } from "@lexical/link"
 import exampleTheme from "themes/exampleTheme"
 import ToolbarPlugin from "plugins/ToolbarPlugin"
+import AutoLinkPlugin from "plugins/AutoLinkPlugin"
 
 // When the editor changes, you can get notified via the
 // LexicalOnChangePlugin!
@@ -47,11 +55,22 @@ const onError = (error: Error) => {
   console.error(error)
 }
 
+const nodes = [
+  HeadingNode,
+  QuoteNode,
+  CodeHighlightNode,
+  CodeNode,
+  LinkNode,
+  ListItemNode,
+  ListNode,
+]
+
 const Editor = () => {
   const initialConfig = {
     namespace: "MyEditor",
     theme: exampleTheme,
     onError,
+    nodes,
   }
 
   return (
@@ -66,6 +85,8 @@ const Editor = () => {
               <div className="editor-placeholder">Enter some text...</div>
             }
           />
+          <ListPlugin />
+          <LinkPlugin />
           <OnChangePlugin onChange={onChange} />
           <HistoryPlugin />
           <MyCustomAutoFocusPlugin />
