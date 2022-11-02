4bea89a3e3974bc0145fee09054882aa312431b5
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-01 19:55:32 -0500

    feat: add property for extending editor theme

--- src/EditorBase.tsx
+++ src/EditorBase.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import type { LexicalNode, Klass } from "lexical"
+import type { LexicalNode, Klass, EditorThemeClasses } from "lexical"
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
 import { ContentEditable } from "@lexical/react/LexicalContentEditable"
 import { Container, Paper } from "@material-ui/core"
@@ -10,10 +10,16 @@ type EditorBaseProperties = {
   children?: React.ReactElement | React.ReactElement[]
   // eslint-disable-next-line react/require-default-props
   nodes?: Klass<LexicalNode>[]
+  // eslint-disable-next-line react/require-default-props
+  theme?: EditorThemeClasses
 }
 
-const EditorBase = ({ children = [], nodes = [] }: EditorBaseProperties) => (
-  <EditorComposer nodes={nodes}>
+const EditorBase = ({
+  children = [],
+  nodes = [],
+  theme = {},
+}: EditorBaseProperties) => (
+  <EditorComposer nodes={nodes} theme={theme}>
     <Container>
       {children}
       <Paper style={{ position: "relative" }}>
