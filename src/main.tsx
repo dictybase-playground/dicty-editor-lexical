c115b13fed41d7af2d409446daa2e297afa4b838
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-08 10:56:35 -0600

    feat: switch to react 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./styles.css"
 import App from "./App"
@@ -11,8 +11,9 @@ const router = createBrowserRouter([
   },
 ])
 
-ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
+  document.querySelector("#root"),
 )
