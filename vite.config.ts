233f5a87f3181e22c62ef0764b4e27e980892178
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-07 18:39:00 -0600

    feat: add swc react refresh plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
-
+import { swcReactRefresh } from "vite-plugin-swc-react-refresh"
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), swcReactRefresh(), react()],
+  esbuild: { jsx: "automatic" },
   server: {
     port: 3000,
   },
