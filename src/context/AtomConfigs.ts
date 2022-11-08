9eb79d99f5300322f39ee68ef5e5a94d4d9f9af7
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-08 10:14:16 -0600

    feat: combine format atoms into single object
    combine format atoms into single object and use focusAtom to get derived
    atoms

--- src/context/AtomConfigs.ts
+++ src/context/AtomConfigs.ts
@@ -1,5 +1,65 @@
 import { atom } from "jotai"
+import { focusAtom } from "jotai/optics"
 
-export const isBoldAtom = atom(false)
-export const isItalicAtom = atom(false)
-export const isUnderlinedAtom = atom(false)
+export enum FontFamily {
+  ARIAL = "Arial",
+  COURIER_NEW = "Courier New",
+  GEORGIA = "Georgia",
+  TIMES_NEW_ROMAN = "Times New Roman",
+  TREBUCHET_MS = "Trebuchet MS",
+  VERDANA = "Verdana",
+}
+
+export enum BlockTypes {
+  PARAGRAPH = "paragraph",
+  HEADING_ONE = "h1",
+  HEADING_TWO = "h2",
+  HEADING_THREE = "h3",
+  HEADING_FOUR = "h4",
+  BULLET_LIST = "bullet",
+  NUMBERED_LIST = "number",
+  QUOTE = "quote",
+}
+
+const FontSizes = [
+  "10px",
+  "11px",
+  "12px",
+  "13px",
+  "14px",
+  "15px",
+  "16px",
+  "17px",
+  "18px",
+  "19px",
+  "20px",
+]
+
+export const formatAtom = atom({
+  isBold: false,
+  isItalic: false,
+  isUnderlined: false,
+  fontFamily: FontFamily.ARIAL,
+  fontSize: FontSizes[5],
+})
+
+export const isBoldAtom = focusAtom(formatAtom, (optic) => optic.prop("isBold"))
+export const isItalicAtom = focusAtom(formatAtom, (optic) =>
+  optic.prop("isItalic"),
+)
+export const isUnderlinedAtom = focusAtom(formatAtom, (optic) =>
+  optic.prop("isUnderlined"),
+)
+
+export const fontFamilyAtom = focusAtom(formatAtom, (optic) =>
+  optic.prop("fontFamily"),
+)
+
+export const fontSizeAtom = focusAtom(formatAtom, (optic) =>
+  optic.prop("fontSize"),
+)
+
+export const canUndoAtom = atom(false)
+export const canRedoAtom = atom(false)
+export const isRTLAtom = atom(false)
+export const blockTypesAtom = atom(BlockTypes.PARAGRAPH)
