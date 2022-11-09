d329baca9357d75bd3ca4b3e244b43e274a936d7
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-09 17:18:08 -0600

    style(eslint): turn off unicorn/no-for-of rule

--- src/versions/v2d.tsx
+++ src/versions/v2d.tsx
@@ -1,11 +1,13 @@
 import EditorBase from "EditorBase"
 import ToolbarV2d from "plugins/ToolbarPlugin/versions/ToolbarV2d"
 import PulsePlugin from "plugins/PulsePlugin"
+import TreeViewPlugin from "plugins/TreeViewPlugin"
 
 const EditorV2d = () => (
   <EditorBase>
     <PulsePlugin />
     <ToolbarV2d />
+    <TreeViewPlugin />
   </EditorBase>
 )
 
