07446c004961ee019895e517e3855e961136d529
Siddhartha Basu <biosidd@gmail.com>
2022-11-10 17:53:04 -0600

    refactor: add font size state changes in the toolbar update

--- src/plugins/ToolbarPlugin/versions/useUpdateToolbar2b.tsx
+++ src/plugins/ToolbarPlugin/versions/useUpdateToolbar2b.tsx
@@ -1,19 +1,31 @@
 import { useUpdateAtom } from "jotai/utils"
-import { isBoldAtom, isItalicAtom } from "context/AtomConfigs"
+import {
+  isBoldAtom,
+  isItalicAtom,
+  isUnderlinedAtom,
+  fontSizeAtom,
+} from "context/AtomConfigs"
 import { useCallback } from "react"
 import { $getSelection, $isRangeSelection } from "lexical"
+import { $getSelectionStyleValueForProperty } from "@lexical/selection"
 
 const useUpdateToolbar = () => {
   const setIsBold = useUpdateAtom(isBoldAtom)
   const setIsItalic = useUpdateAtom(isItalicAtom)
+  const setIsUnderlined = useUpdateAtom(isUnderlinedAtom)
+  const setFontSize = useUpdateAtom(fontSizeAtom)
   return useCallback(() => {
     const selection = $getSelection()
 
     if ($isRangeSelection(selection)) {
       setIsBold(selection.hasFormat("bold"))
       setIsItalic(selection.hasFormat("italic"))
+      setIsUnderlined(selection.hasFormat("underline"))
+      setFontSize(
+        $getSelectionStyleValueForProperty(selection, "font-size", "15px"),
+      )
     }
-  }, [setIsBold, setIsItalic])
+  }, [setIsBold, setIsItalic, setIsUnderlined, setFontSize])
 }
 
 export default useUpdateToolbar
