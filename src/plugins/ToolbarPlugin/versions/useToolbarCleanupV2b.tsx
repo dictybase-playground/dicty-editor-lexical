94e0f8b56548f0175eab1691855720c1fafe4f76
Siddhartha Basu <biosidd@gmail.com>
2022-11-10 14:19:23 -0600

    docs: add inline documentation for toolbar cleanup useEffect block

--- src/plugins/ToolbarPlugin/versions/useToolbarCleanupV2b.tsx
+++ src/plugins/ToolbarPlugin/versions/useToolbarCleanupV2b.tsx
@@ -7,6 +7,18 @@ const LowPriority = 1
 const useToolbarCleanup = () => {
   const updateToolbar = useUpdateToolbar()
   const [editor] = useLexicalComposerContext()
+  /*
+    1. In a useEffect callback, the returned function is called after every
+    render to cleanup the side effects of the previous render.
+
+    2. Here, the returned function is the result of calling mergeRegister,
+    a function that calls each function provided to as an argument.
+
+    3. Since the editor's registerListener and registerCommand methods return
+    a function that removes the listeners they register, mergeRegister will
+    return a function that cleans up the listeners registered from a
+    previous render.
+  */
   return useEffect(() => {
     const unregisterUpdateListener = editor.registerUpdateListener(
       ({ editorState }) => {
