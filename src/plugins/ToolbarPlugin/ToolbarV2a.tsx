5d585a00ba2fad77148019ca5c2702f7bfd69934
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-11 15:08:43 -0600

    refactor(ToolbarV2a): use toolbarcleanup hook in toolbarv2a

--- src/plugins/ToolbarPlugin/ToolbarV2a.tsx
+++ src/plugins/ToolbarPlugin/ToolbarV2a.tsx
@@ -1,48 +1,10 @@
-import { useEffect } from "react"
-import { SELECTION_CHANGE_COMMAND } from "lexical"
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import { mergeRegister } from "@lexical/utils"
 import useToolbarStyles from "styles/ToolbarStyles"
 import { Divider, FormatBoldButton } from "components"
-import useUpdateToolbar from "hooks/useUpdateToolbar2a"
-
-const LowPriority = 1
+import useToolbarCleanup from "hooks/useToolbarCleanupV2a"
 
 const ToolbarV2 = () => {
-  const [editor] = useLexicalComposerContext()
-  const updateToolbar = useUpdateToolbar()
+  useToolbarCleanup()
   const toolbarClasses = useToolbarStyles()
-  /*
-    1. In a useEffect callback, the returned function is called after every
-    render to cleanup the side effects of the previous render.
-
-    2. Here, the returned function is the result of calling mergeRegister,
-    a function that calls each function provided to as an argument.
-
-    3. Since the editor's registerListener and registerCommand methods return
-    a function that removes the listeners they register, mergeRegister will
-    return a function that cleans up the listeners registered from a
-    previous render.
-  */
-  useEffect(
-    () =>
-      mergeRegister(
-        editor.registerUpdateListener(({ editorState }) => {
-          editorState.read(() => {
-            updateToolbar()
-          })
-        }),
-        editor.registerCommand(
-          SELECTION_CHANGE_COMMAND,
-          () => {
-            updateToolbar()
-            return false
-          },
-          LowPriority,
-        ),
-      ),
-    [editor, updateToolbar],
-  )
 
   return (
     <div className={toolbarClasses.root}>
--- src/plugins/ToolbarPlugin/ToolbarV2b.tsx
+++ src/plugins/ToolbarPlugin/ToolbarV2b.tsx
@@ -5,6 +5,7 @@ import useToolbarCleanup from "hooks/useToolbarCleanupV2b"
 const ToolbarV2 = () => {
   useToolbarCleanup()
   const toolbarClasses = useToolbarStyles()
+
   return (
     <div className={toolbarClasses.root}>
       <Divider />
