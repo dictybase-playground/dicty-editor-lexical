63a8bac8a2a2d10ffe38dcd788e70cba396c2e09
Siddhartha Basu <biosidd@gmail.com>
2022-11-11 08:30:28 -0600

    feat: add a new editor with fontsize in the toolbar

---
+++ src/versions/v3a.tsx
@@ -0,0 +1,10 @@
+import EditorBase from "EditorBase"
+import ToolbarV3a from "plugins/ToolbarPlugin/versions/ToolbarV3a"
+
+const EditorV3a = () => (
+  <EditorBase>
+    <ToolbarV3a />
+  </EditorBase>
+)
+
+export default EditorV3a
