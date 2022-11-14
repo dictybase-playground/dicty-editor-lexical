d2ecbfe4cee77631804e7a944da4003a73a86368
Siddhartha Basu <biosidd@gmail.com>
2022-11-14 12:59:34 -0600

    fix: use the correct import

--- src/hooks/useActiveClass.ts
+++ src/hooks/useActiveClass.ts
@@ -2,7 +2,7 @@ import joinClasses from "utils/joinClasses"
 import { useAtomValue } from "jotai"
 import type { WritableAtom } from "jotai"
 import type { SetStateAction } from "react"
-import useButtonStyles from "../utils/ToolBarButtonStyles"
+import useButtonStyles from "../utils/ToolBarItemStyles"
 
 const useActiveClass = (
   atomConfig: WritableAtom<boolean, SetStateAction<boolean>, void>,
