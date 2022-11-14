e2adff868a434b59bec926c9d2bc058bc794a3ae
Siddhartha Basu <biosidd@gmail.com>
2022-11-14 13:00:03 -0600

    feat: add the new toolbar and page with font family dropdown

---
+++ src/pages/v3b.tsx
@@ -0,0 +1,10 @@
+import EditorBase from "components/EditorBase"
+import ToolbarV3b from "plugins/ToolbarV3b"
+
+const EditorV3b = () => (
+  <EditorBase>
+    <ToolbarV3b />
+  </EditorBase>
+)
+
+export default EditorV3b
