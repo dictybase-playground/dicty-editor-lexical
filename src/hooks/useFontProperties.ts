2b10b0ad9dd87f67722e44619cf9db64a1627556
Siddhartha Basu <biosidd@gmail.com>
2022-11-14 09:26:31 -0600

    feat: add useFontProperties hook that sets up various properties of fonts

---
+++ src/hooks/useFontProperties.ts
@@ -0,0 +1,29 @@
+import { useUpdateAtom } from "jotai/utils"
+import {
+  isBoldAtom,
+  isItalicAtom,
+  isUnderlinedAtom,
+  fontSizeAtom,
+} from "context/AtomConfigs"
+import { useCallback } from "react"
+import { $getSelection, $isRangeSelection } from "lexical"
+import { $getSelectionStyleValueForProperty } from "@lexical/selection"
+
+const useFontProperties = () => {
+  const setIsBold = useUpdateAtom(isBoldAtom)
+  const setIsItalic = useUpdateAtom(isItalicAtom)
+  const setIsUnderlined = useUpdateAtom(isUnderlinedAtom)
+  const setFontSize = useUpdateAtom(fontSizeAtom)
+  return useCallback(() => {
+    const selection = $getSelection()
+    if (!$isRangeSelection(selection)) return
+    setIsBold(selection.hasFormat("bold"))
+    setIsItalic(selection.hasFormat("italic"))
+    setIsUnderlined(selection.hasFormat("underline"))
+    setFontSize(
+      $getSelectionStyleValueForProperty(selection, "font-size", "15px"),
+    )
+  }, [setIsBold, setIsItalic, setIsUnderlined, setFontSize])
+}
+
+export default useFontProperties
