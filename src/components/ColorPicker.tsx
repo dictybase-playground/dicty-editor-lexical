9e040e7bf4b2b6f36f6b1769607fe616f2ad903d
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-15 16:36:36 -0600

    refactor: move color picker style hook to separate file'

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -5,25 +5,14 @@ import { HexColorPicker } from "react-colorful"
 import { ClickAwayListener, makeStyles } from "@material-ui/core"
 import { textColorAtom } from "context/AtomConfigs"
 import applyStyleText from "utils/textStyles"
+import useColorPickerStyles from "utils/ColorPickerStyles"
 import ColorPickerButton from "./ColorPickerButton"
-
-const useStyles = makeStyles({
-  dropdown: {
-    position: "absolute",
-    width: "min-content",
-    top: 30,
-    zIndex: 1,
-    borderRadius: "8px",
-    boxShadow:
-      "0 12px 28px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0 rgba(0, 0, 0, 0.1), inset 0 0 0 1px rgba(255, 255, 255, 0.5)",
-  },
-})
 
 const ColorPicker = () => {
   const [editor] = useLexicalComposerContext()
   const [color, setColor] = useAtom(textColorAtom)
   const [isOpen, setIsOpen] = useState(false)
-  const { dropdown } = useStyles()
+  const { root } = useColorPickerStyles()
 
   const onClose = () => {
     if (!isOpen) return
@@ -39,7 +28,7 @@ const ColorPicker = () => {
       <div style={{ position: "relative" }}>
         <ColorPickerButton onClick={() => setIsOpen(!isOpen)} />
         {isOpen && (
-          <div className={dropdown}>
+          <div className={root}>
             <HexColorPicker color={color} onChange={setColor} />
           </div>
         )}
