03a7e62e93bac7ae257348f304a8a05ffa0b4863
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-14 19:34:53 -0600

    feat: implement color picker

---
+++ src/pages/v4.tsx
@@ -0,0 +1,10 @@
+import EditorBase from "components/EditorBase"
+import ToolbarV4 from "plugins/ToolbarV4"
+
+const EditorV4 = () => (
+  <EditorBase>
+    <ToolbarV4 />
+  </EditorBase>
+)
+
+export default EditorV4
