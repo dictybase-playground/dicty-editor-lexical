e37ab6db1fd225b711f88b3eb001dbdf72774746
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-15 14:24:32 -0600

    refactor(ColorPickerButton.tsx): wrap color picker components in clickawaylistener

--- src/components/ColorPickerButton.tsx
+++ src/components/ColorPickerButton.tsx
@@ -1,19 +1,37 @@
-import { useState, useRef } from "react"
+import { useState, useRef, useEffect } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 import { useAtom } from "jotai"
 import { HexColorPicker } from "react-colorful"
-import { IconButton, Menu, SvgIcon } from "@material-ui/core"
+import {
+  IconButton,
+  ClickAwayListener,
+  SvgIcon,
+  makeStyles,
+} from "@material-ui/core"
 import FormatColorTextIcon from "@material-ui/icons/FormatColorText"
 import { textColorAtom } from "context/AtomConfigs"
 import useToolbarItemStyles from "utils/ToolBarItemStyles"
 import applyStyleText from "utils/textStyles"
 
+const useStyles = makeStyles({
+  root: {
+    position: "relative",
+  },
+  dropdown: {
+    position: "absolute",
+    width: "min-content",
+    top: 30,
+
+    zIndex: 1,
+  },
+})
+
 const ColorPickerButton = () => {
   const [editor] = useLexicalComposerContext()
   const [color, setColor] = useAtom(textColorAtom)
   const [isOpen, setIsOpen] = useState(false)
-  const buttonReference = useRef(null)
   const itemClass = useToolbarItemStyles()
+  const { dropdown } = useStyles()
 
   const onClose = () => {
     applyStyleText(editor, { color })
@@ -21,26 +39,27 @@ const ColorPickerButton = () => {
   }
 
   return (
-    <>
-      <IconButton
-        size="small"
-        ref={buttonReference}
-        className={itemClass.root}
-        aria-controls="color-picker"
-        onClick={() => setIsOpen(!isOpen)}>
-        <SvgIcon fontSize="small" htmlColor={color}>
-          <FormatColorTextIcon />
-        </SvgIcon>
-      </IconButton>
-
-      <Menu
-        id="color-picker"
-        anchorEl={buttonReference.current}
-        open={isOpen}
-        onClose={onClose}>
-        <HexColorPicker color={color} onChange={setColor} />
-      </Menu>
-    </>
+    <ClickAwayListener
+      mouseEvent="onMouseDown"
+      touchEvent="onTouchStart"
+      onClickAway={onClose}>
+      <div style={{ position: "relative" }}>
+        <IconButton
+          size="small"
+          className={itemClass.root}
+          aria-controls="color-picker"
+          onClick={() => setIsOpen(!isOpen)}>
+          <SvgIcon fontSize="small" htmlColor={color}>
+            <FormatColorTextIcon />
+          </SvgIcon>
+        </IconButton>
+        {isOpen && (
+          <div className={dropdown}>
+            <HexColorPicker color={color} onChange={setColor} />
+          </div>
+        )}
+      </div>
+    </ClickAwayListener>
   )
 }
 
