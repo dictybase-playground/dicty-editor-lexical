ab34a044cd1e148e4f142b13206deb762ca17a8c
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-22 16:52:45 -0600

    feat: create block format dropdown component

---
+++ src/pages/v6.tsx
@@ -0,0 +1,12 @@
+import EditorBase from "components/EditorBase"
+import ToolbarV5 from "plugins/ToolbarV5"
+import Actions from "components/Actions"
+
+const EditorV6 = () => (
+  <EditorBase>
+    <Actions />
+    <ToolbarV5 />
+  </EditorBase>
+)
+
+export default EditorV6
