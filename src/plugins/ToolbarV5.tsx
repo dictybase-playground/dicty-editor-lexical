6321b065812af17162221d4fed7ab78fc2863c5e
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-22 16:52:45 -0600

    feat: create block format dropdown component

---
+++ src/plugins/ToolbarV5.tsx
@@ -0,0 +1,33 @@
+import {
+  Divider,
+  FormatBoldButton,
+  FormatItalicButton,
+  FormatUnderlineButton,
+  FontSizeDropdown,
+} from "components"
+import FontFamilyDropdown from "components/FontFamilyDropdown"
+import BlockFormatDropdown from "components/BlockFormatDropdown"
+import ColorPicker from "components/ColorPicker"
+import useToolbarStyles from "utils/ToolbarStylesV2"
+import ToolBar from "@material-ui/core/Toolbar"
+import useCleanup from "../hooks/useCleanup"
+
+const ToolbarV4 = () => {
+  useCleanup()
+  const toolbarClasses = useToolbarStyles()
+
+  return (
+    <ToolBar variant="dense" className={toolbarClasses.root}>
+      <Divider />
+      <FontSizeDropdown />
+      <FontFamilyDropdown />
+      <BlockFormatDropdown />
+      <FormatBoldButton />
+      <FormatItalicButton />
+      <FormatUnderlineButton />
+      <ColorPicker />
+    </ToolBar>
+  )
+}
+
+export default ToolbarV4
