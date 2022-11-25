789bf44b192193166580173e17db89720ce5c47f
Kevin Tun <kevin.tun@northwestern.edu>
2022-11-25 16:42:44 -0600

    refactor: separate effect and onclick definition to own hook

--- src/components/RedoButton.tsx
+++ src/components/RedoButton.tsx
@@ -1,35 +1,14 @@
-import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import { CAN_REDO_COMMAND, REDO_COMMAND } from "lexical"
 import { IconButton } from "@material-ui/core"
 import { Redo } from "@material-ui/icons"
-import { useAtom } from "jotai"
+import { useAtomValue } from "jotai/utils"
 import { canRedoAtom } from "context/AtomConfigs"
-import { useCallback, useEffect } from "react"
 import useToolbarItemStyles from "utils/ToolbarItemStyles"
+import useRedo from "hooks/useRedo"
 
 const RedoButton = () => {
-  const [editor] = useLexicalComposerContext()
-  const [canRedo, setCanRedo] = useAtom(canRedoAtom)
+  const canRedo = useAtomValue(canRedoAtom)
   const classes = useToolbarItemStyles()
-
-  useEffect(() => {
-    const unregisterCanUndoCommand = editor.registerCommand(
-      CAN_REDO_COMMAND,
-      (payload) => {
-        setCanRedo(payload)
-        return false
-      },
-      1,
-    )
-
-    return () => {
-      unregisterCanUndoCommand()
-    }
-  })
-
-  const onClick = useCallback(() => {
-    editor.dispatchCommand(REDO_COMMAND, undefined)
-  }, [editor])
+  const onClick = useRedo()
 
   return (
     <IconButton
@@ -37,8 +16,7 @@ const RedoButton = () => {
       className={classes.root}
       onClick={onClick}
       title="Redo"
-      aria-label="Redo"
-      type="button">
+      aria-label="Redo">
       <Redo />
     </IconButton>
   )
