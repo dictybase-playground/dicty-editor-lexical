78caf4d0798db74b12066a9050eb2d3e6df28f56
Siddhartha Basu <biosidd@gmail.com>
2022-12-07 13:42:20 -0600

    refactor: return the setter function from useToggle hook

--- src/hooks/useToogle.ts
+++ src/hooks/useToogle.ts
@@ -1,9 +1,11 @@
-import { useCallback, useState } from "react"
+import { useCallback, useState, Dispatch, SetStateAction } from "react"
 
-const useToggle = (initialState?: boolean) => {
+const useToggle = (
+  initialState?: boolean,
+): [boolean, () => void, Dispatch<SetStateAction<boolean>>] => {
   const [value, setValue] = useState(!!initialState)
   const toggle = useCallback(() => setValue((x) => !x), [])
-  return [value, toggle]
+  return [value, toggle, setValue]
 }
 
 export default useToggle
