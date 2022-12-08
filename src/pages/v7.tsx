fe8913f95e67eb6e6ad9974ca23e4d21dcebfe9e
Kevin Tun <kevin.tun@northwestern.edu>
2022-12-07 22:34:43 -0600

    refactor: combine editor components into single component

--- src/pages/v7.tsx
+++ src/pages/v7.tsx
@@ -1,16 +1,62 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer"
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
 import { ListPlugin } from "@lexical/react/LexicalListPlugin"
-import EditorBase from "components/EditorBase"
-import ToolbarV6 from "plugins/ToolbarV6"
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin"
+import { ContentEditable } from "@lexical/react/LexicalContentEditable"
+import { ListItemNode, ListNode } from "@lexical/list"
+import { HeadingNode, QuoteNode } from "@lexical/rich-text"
+import { Container, Paper } from "@material-ui/core"
+import ToolbarV6Plugin from "plugins/ToolbarV6"
 import Actions from "components/Actions"
+import {
+  useEditorInputStyles,
+  useEditorPlaceholderStyles,
+} from "hooks/useEditorStyles"
+import "styles/editor.css"
 
-const EditorV6 = () => (
-  <EditorBase>
-    <HistoryPlugin />
-    <ListPlugin />
-    <Actions />
-    <ToolbarV6 />
-  </EditorBase>
-)
+const editorTheme = {
+  paragraph: "editor-paragraph",
+  text: {
+    bold: "editor-text-bold",
+    italic: "editor-text-italic",
+    underline: "editor-text-underline",
+  },
+}
 
-export default EditorV6
+const onError = (error: Error) => {
+  // eslint-disable-next-line no-console
+  console.error(error)
+}
+
+const initialConfig = {
+  namespace: "DictyEditor",
+  theme: { ...editorTheme },
+  nodes: [HeadingNode, QuoteNode, ListItemNode, ListNode],
+  onError,
+}
+
+const EditorV7 = () => {
+  const inputClasses = useEditorInputStyles()
+  const placeholderClasses = useEditorPlaceholderStyles()
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <Container>
+        <HistoryPlugin />
+        <ListPlugin />
+        <ToolbarV6Plugin />
+        <Paper style={{ position: "relative" }}>
+          <RichTextPlugin
+            contentEditable={<ContentEditable className={inputClasses.root} />}
+            placeholder={
+              <div className={placeholderClasses.root}>Enter some text...</div>
+            }
+          />
+        </Paper>
+      </Container>
+      <Actions />
+    </LexicalComposer>
+  )
+}
+
+export default EditorV7
