2af04a264cd67613b745d6738f0c7842933d5a4c
Siddhartha Basu <biosidd@gmail.com>
2022-12-15 12:27:12 -0600

    build: use a separate config file with the custom base path

---
+++ viteGithubConfig.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { swcReactRefresh } from "vite-plugin-swc-react-refresh"
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), swcReactRefresh()],
+  esbuild: { jsx: "automatic" },
+  base: "/dicty-editor-lexical/",
+})
