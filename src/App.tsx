ba838272b23620710c64949a14cb5eececd70767
Siddhartha Basu <biosidd@gmail.com>
2022-12-15 17:59:30 -0600

    refactor: replace routes with direct deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,4 @@
-import { Fragment } from "react"
-import { Routes, Route } from "react-router-dom"
 import EditorV7 from "pages/v7"
-import routes from "./routes"
 
-const App = () => (
-  <Routes>
-    <Route index element={<EditorV7 />} />
-    {routes.map(({ path, component: Component = Fragment }) => (
-      <Route key={path} path={path} element={<Component />} />
-    ))}
-  </Routes>
-)
-
+const App = () => <EditorV7 />
 export default App
