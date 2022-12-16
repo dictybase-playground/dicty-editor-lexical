9ad5ece16a8d15a7aecc4294467fdf87779b56d0
Siddhartha Basu <biosidd@gmail.com>
2022-12-15 18:25:05 -0600

    refactor: add back the all the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
+import { Fragment } from "react"
+import { Routes, Route } from "react-router-dom"
 import EditorV7 from "pages/v7"
+import routes from "./routes"
 
-const App = () => <EditorV7 />
+const App = () => (
+  <Routes>
+    <Route index element={<EditorV7 />} />
+    {routes.map(({ path, component: Component = Fragment }) => (
+      <Route key={path} path={path} element={<Component />} />
+    ))}
+  </Routes>
+)
+
 export default App
