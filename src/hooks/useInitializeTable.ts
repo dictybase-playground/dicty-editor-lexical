8502a97738270ba76f03af79d9ef8a0f2a7cd5e6
Kevin Tun <kevin.tun@northwestern.edu>
2023-02-13 15:10:00 -0600

    refactor(initializeTable): split up nested functions

--- src/hooks/useInitializeTable.ts
+++ src/hooks/useInitializeTable.ts
@@ -1,91 +1,36 @@
 import { useEffect } from "react"
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
-import {
-  TableNode,
-  $isTableNode,
-  applyTableHandlers,
-  HTMLTableElementWithWithTableSelectionState,
-  TableSelection,
-} from "@lexical/table"
-import { $nodesOfType, $getNodeByKey, NodeMutation } from "lexical"
-import { pipe, A, G } from "@mobily/ts-belt"
+import { TableNode, TableSelection } from "@lexical/table"
+import { $nodesOfType } from "lexical"
+import { A } from "@mobily/ts-belt"
 import CustomTableNode from "nodes/CustomTableNode"
-
-const isCreated = ([, mutation]: [string, NodeMutation]) =>
-  mutation === "created"
-
-const isDestroyed = ([, mutation]: [string, NodeMutation]) =>
-  mutation === "destroyed"
+import {
+  getInitializationFunction,
+  getMutationHandler,
+} from "utils/initializeTable"
 
 const useInitializeTable = () => {
   const [editor] = useLexicalComposerContext()
   useEffect(() => {
     const tableSelections = new Map<string, TableSelection>()
 
-    const initializeTableNode = (tableNode: TableNode) => {
-      const nodeKey = tableNode.getKey()
-      const tableElement = editor.getElementByKey(
-        nodeKey,
-      ) as HTMLTableElementWithWithTableSelectionState
-
-      if (tableElement && !tableSelections.has(nodeKey)) {
-        const tableSelection = applyTableHandlers(
-          tableNode,
-          tableElement,
-          editor,
-        )
-        tableSelections.set(nodeKey, tableSelection)
-      }
-    } // Plugins might be loaded _after_ initial content is set, hence existing table nodes
+    // Plugins might be loaded _after_ initial content is set, hence existing table nodes
     // won't be initialized from mutation[create] listener. Instead doing it here,
+
+    const unregisterMutationListener = editor.registerMutationListener(
+      CustomTableNode,
+      getMutationHandler(tableSelections, editor),
+    )
 
     editor.getEditorState().read(() => {
       const tableNodes = $nodesOfType(TableNode)
 
       tableNodes.map((tableNode) => {
-        initializeTableNode(tableNode)
+        getInitializationFunction(tableSelections, editor)(tableNode)
         return tableNode
       })
     })
 
-    const unregisterMutationListener = editor.registerMutationListener(
-      CustomTableNode,
-      (nodeMutations) => {
-        const getNode = (nodeKey: string) =>
-          editor.getEditorState().read(() => $getNodeByKey(nodeKey))
-
-        pipe(
-          [...nodeMutations],
-          A.filter(isCreated),
-          A.map(A.head),
-          A.filter(G.isString),
-          A.map(getNode),
-          A.filter($isTableNode),
-          A.tap(initializeTableNode),
-        )
-
-        const getTableSelection = ([nodeKey]: [string, NodeMutation]): [
-          string,
-          TableSelection | undefined,
-        ] => [nodeKey, tableSelections.get(nodeKey)]
-
-        const cleanUp = ([nodeKey, tableSelection]: [
-          string,
-          TableSelection | undefined,
-        ]) => {
-          if (!tableSelection) return
-          tableSelection.removeListeners()
-          tableSelections.delete(nodeKey)
-        }
-
-        pipe(
-          [...nodeMutations],
-          A.filter(isDestroyed),
-          A.map(getTableSelection),
-          A.tap(cleanUp),
-        )
-      },
-    )
     return () => {
       unregisterMutationListener() // Hook might be called multiple times so cleaning up tables listeners as well,
       // as it'll be reinitialized during recurring call
