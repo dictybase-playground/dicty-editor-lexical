7ac91868b71aceaf298271506ddb55f73eebebf6
Kevin Tun <kevin.tun@northwestern.edu>
2023-02-14 18:04:14 -0600

    refactor(create width table test): lift set up to describe block, reduce redundancy

--- src/utils/$createWidthTable.test.tsx
+++ src/utils/$createWidthTable.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect, afterEach } from "vitest"
+import { describe, test, expect, afterEach } from "vitest"
 import { createEditor, ParagraphNode, TextNode } from "lexical"
 import { testConfig } from "components/LexicalTestComposer"
 import $createWidthTable, {
@@ -33,173 +33,142 @@ afterEach(() => {
 })
 
 describe("CreateParagraphWithTextNode", () => {
-  it("creates and returns a paragraph node", () => {
-    let paragraphNode
-    testEditor.update(() => {
-      paragraphNode = createParagraphWithTextNode()
-    })
-    expect(paragraphNode).toBeInstanceOf(ParagraphNode)
+  let paragraphNode: ParagraphNode
+  let textNode: TextNode | null
+  testEditor.update(() => {
+    paragraphNode = createParagraphWithTextNode()
+    textNode = paragraphNode.getFirstChild()
   })
 
-  it("it returns a paragraph node with a Text Node as an immediate child", () => {
-    let paragraphNode
-    let textNode
-    testEditor.update(() => {
-      paragraphNode = createParagraphWithTextNode()
-      textNode = paragraphNode.getFirstChild()
-    })
+  test("creates and returns a paragraph node", () => {
+    expect(paragraphNode).toBeInstanceOf(ParagraphNode)
+  })
+  test("it returns a paragraph node with a Text Node as an immediate child", () => {
     expect(textNode).toBeInstanceOf(TextNode)
   })
 })
 
-describe("createCellWithParagraphNode", () => {
-  it("returns a table cell node", () => {
-    let tableCellNode
-    testEditor.update(() => {
-      tableCellNode = createCellWithParagraphNode()
-    })
-    expect(tableCellNode).toBeInstanceOf(TableCellNode)
+describe("createCellWithParagraphNode & createHeaderCellwithParagraphNode", () => {
+  let bodyCellNode: TableCellNode
+  let headerCellNode: TableCellNode
+  let bodyCellParagraphNode: ParagraphNode | null
+  let headerCellParagraphNode: ParagraphNode | null
+  let bodyCellHeaderState: number
+  let headerCellHeaderState: number
+  testEditor.update(() => {
+    bodyCellNode = createCellWithParagraphNode()
+    bodyCellParagraphNode = bodyCellNode.getFirstChild()
+    bodyCellHeaderState = createCellWithParagraphNode().getHeaderStyles()
+    headerCellNode = createHeaderCellWithParagraphNode()
+    headerCellParagraphNode = headerCellNode.getFirstChild()
+    headerCellHeaderState =
+      createHeaderCellWithParagraphNode().getHeaderStyles()
+  })
+  test("returns a table cell node", () => {
+    expect(bodyCellNode).toBeInstanceOf(TableCellNode)
+    expect(headerCellNode).toBeInstanceOf(TableCellNode)
   })
 
-  it("returns a table cell node with a paragraph node as an immediate child", () => {
-    let tableCellNode
-    let paragraphNode
-    testEditor.update(() => {
-      tableCellNode = createCellWithParagraphNode()
-      paragraphNode = tableCellNode.getFirstChild()
-    })
-    expect(paragraphNode).toBeInstanceOf(ParagraphNode)
+  test("returns a table cell node with a paragraph node as an immediate child", () => {
+    expect(bodyCellParagraphNode).toBeInstanceOf(ParagraphNode)
+    expect(headerCellParagraphNode).toBeInstanceOf(ParagraphNode)
   })
 
-  it("returns a table cell node that is not a header cell", () => {
-    let headerState
-    testEditor.update(() => {
-      headerState = createCellWithParagraphNode().getHeaderStyles()
-    })
-    expect(headerState).toEqual(TableCellHeaderStates.NO_STATUS)
-  })
-})
-
-describe("createHeaderCellWithParagraphNode", () => {
-  it("returns a table cell node", () => {
-    let tableCellNode
-    testEditor.update(() => {
-      tableCellNode = createHeaderCellWithParagraphNode()
-    })
-    expect(tableCellNode).toBeInstanceOf(TableCellNode)
-  })
-
-  it("returns a table cell node with a paragraph node as an immediate child", () => {
-    let tableCellNode
-    let paragraphNode
-    testEditor.update(() => {
-      tableCellNode = createHeaderCellWithParagraphNode()
-      paragraphNode = tableCellNode.getFirstChild()
-    })
-    expect(paragraphNode).toBeInstanceOf(ParagraphNode)
-  })
-
-  it("returns a table cell node that is a header cell", () => {
-    let headerState
-    testEditor.update(() => {
-      headerState = createHeaderCellWithParagraphNode().getHeaderStyles()
-    })
-    expect(headerState).toEqual(TableCellHeaderStates.ROW)
+  test("returns a table cell node with the appropriate header state", () => {
+    expect(bodyCellHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
+    expect(headerCellHeaderState).toEqual(TableCellHeaderStates.ROW)
   })
 })
 
-describe("bodyCellsToAppend", () => {
-  it("returns a function that, when called, appends a specified number of body cells to a table row ", () => {
-    const cellCount = Math.floor(Math.random() * 10)
+describe("bodyCellsToAppend & headerCellsToAppend", () => {
+  const cellCount = Math.floor(Math.random() * 10)
 
-    let tableRow
-    let cellsInRow
-    let firstCell
-    let firstCellHeaderState
-    const appendNCellsFunction = bodyCellsToAppend(cellCount)
+  let tableBodyRow: TableRowNode
+  let bodyCellsInRow: number
+  let firstBodyCell: TableCellNode | null
+  let firstBodyCellHeaderState: number | null
+  const appendNBodyCellsFunction = bodyCellsToAppend(cellCount)
 
-    testEditor.update(() => {
-      tableRow = new TableRowNode()
-      appendNCellsFunction(tableRow)
-      cellsInRow = tableRow.getChildrenSize()
-      firstCell = tableRow.getFirstChild()
-      firstCellHeaderState = firstCell ? firstCell.getHeaderStyles() : null
-    })
+  let tableHeaderRow: TableRowNode
+  let headerCellsInRow: number
+  let firstHeaderCell: TableCellNode | null
+  let firstHeaderCellHeaderState: number | null
+  const appendNHeaderCellsFunction = headerCellsToAppend(cellCount)
 
-    expect(firstCell).toBeInstanceOf(TableCellNode)
-    expect(firstCellHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
-    expect(cellsInRow).toEqual(cellCount)
+  testEditor.update(() => {
+    tableBodyRow = new TableRowNode()
+    appendNBodyCellsFunction(tableBodyRow)
+    bodyCellsInRow = tableBodyRow.getChildrenSize()
+    firstBodyCell = tableBodyRow.getFirstChild()
+    firstBodyCellHeaderState = firstBodyCell
+      ? firstBodyCell.getHeaderStyles()
+      : null
+
+    tableHeaderRow = new TableRowNode()
+    appendNHeaderCellsFunction(tableHeaderRow)
+    headerCellsInRow = tableHeaderRow.getChildrenSize()
+    firstHeaderCell = tableHeaderRow.getFirstChild()
+    firstHeaderCellHeaderState = firstHeaderCell
+      ? firstHeaderCell.getHeaderStyles()
+      : null
+  })
+  test("returns a function that, when called, appends a specified number of body cells to a table row ", () => {
+    expect(firstBodyCell).toBeInstanceOf(TableCellNode)
+    expect(firstBodyCellHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
+    expect(bodyCellsInRow).toEqual(cellCount)
+
+    expect(firstHeaderCell).toBeInstanceOf(TableCellNode)
+    expect(firstHeaderCellHeaderState).toEqual(TableCellHeaderStates.ROW)
+    expect(headerCellsInRow).toEqual(cellCount)
   })
 })
 
-describe("headerCellsToAppend", () => {
-  it("returns a function that, when called, appends a specified number of header cells to a table row ", () => {
-    const cellCount = Math.floor(Math.random() * 10)
-    let tableRow
-    let cellsInRow
-    let firstCell
-    let firstCellHeaderState
-    const appendNCellsFunction = headerCellsToAppend(cellCount)
-
-    testEditor.update(() => {
-      tableRow = new TableRowNode()
-      appendNCellsFunction(tableRow)
-      cellsInRow = tableRow.getChildrenSize()
-      firstCell = tableRow.getFirstChild()
-      firstCellHeaderState = firstCell ? firstCell.getHeaderStyles() : null
-    })
-
-    expect(firstCell).toBeInstanceOf(TableCellNode)
-    expect(firstCellHeaderState).toEqual(TableCellHeaderStates.ROW)
-    expect(cellsInRow).toEqual(cellCount)
-  })
-})
-
-describe("cellsToAppend", () => {
+describe("cellsToAppend & headerCellsToAppend", () => {
   const cellCount = 2
   const appendThreeCellsToEachRow = cellsToAppend(cellCount)
+  let rowArray
+  let firstRowChildren: TableCellNode[]
+  let firstCellRowOne: TableCellNode
+  let secondCellRowOne: TableCellNode
+  let firstCellRowOneHeaderState: number
+  let secondCellRowOneHeaderState: number
 
-  it("returns a function that, when passed an array of table rows, appends header cells to the first row", () => {
-    let rowArray
-    let firstRowChildren
-    let firstCell
-    let firstCellHeaderState
-    let secondCell
-    let secondCellHeaderState
-    testEditor.update(() => {
-      rowArray = Array.from({ length: 2 }).map(() => $createTableRowNode())
-      appendThreeCellsToEachRow(rowArray)
-      firstRowChildren = rowArray[0].getChildren()
-      ;[firstCell, secondCell] = firstRowChildren
-      firstCellHeaderState = firstCell.getHeaderStyles()
-      secondCellHeaderState = secondCell.getHeaderStyles()
-    })
-    expect(firstRowChildren).toHaveLength(cellCount)
-    expect(firstCell).toBeInstanceOf(TableCellNode)
-    expect(secondCell).toBeInstanceOf(TableCellNode)
-    expect(firstCellHeaderState).toEqual(TableCellHeaderStates.ROW)
-    expect(secondCellHeaderState).toEqual(TableCellHeaderStates.ROW)
+  let secondRowChildren: TableCellNode[]
+  let firstCellRowTwo: TableCellNode
+  let secondCellRowTwo: TableCellNode
+  let firstCellRowTwoHeaderState: number
+  let secondCellRowTwoHeaderState: number
+
+  testEditor.update(() => {
+    rowArray = Array.from({ length: 2 }).map(() => $createTableRowNode())
+    appendThreeCellsToEachRow(rowArray)
+    firstRowChildren = rowArray[0].getChildren()
+    ;[firstCellRowOne, secondCellRowOne] = firstRowChildren
+    firstCellRowOneHeaderState = firstCellRowOne.getHeaderStyles()
+    secondCellRowOneHeaderState = secondCellRowOne.getHeaderStyles()
+
+    secondRowChildren = rowArray[1].getChildren()
+    ;[firstCellRowTwo, secondCellRowTwo] = secondRowChildren
+    firstCellRowTwoHeaderState = firstCellRowTwo.getHeaderStyles()
+    secondCellRowTwoHeaderState = secondCellRowTwo.getHeaderStyles()
   })
-  it("returns a function that, when passed an array of table rows, appends body cells to subsequent rows", () => {
-    let rowArray
-    let secondRowChildren
-    let firstCell
-    let firstCellHeaderState
-    let secondCell
-    let secondCellHeaderState
-    testEditor.update(() => {
-      rowArray = Array.from({ length: 2 }).map(() => $createTableRowNode())
-      appendThreeCellsToEachRow(rowArray)
-      secondRowChildren = rowArray[1].getChildren()
-      ;[firstCell, secondCell] = secondRowChildren
-      firstCellHeaderState = firstCell.getHeaderStyles()
-      secondCellHeaderState = secondCell.getHeaderStyles()
-    })
+
+  test("returns a function that, when passed an array of table rows, appends a given number of schildren to each row", () => {
+    expect(firstRowChildren).toHaveLength(cellCount)
     expect(secondRowChildren).toHaveLength(cellCount)
-    expect(firstCell).toBeInstanceOf(TableCellNode)
-    expect(secondCell).toBeInstanceOf(TableCellNode)
-    expect(firstCellHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
-    expect(secondCellHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
+  })
+  test("the appended children of the rows are TableCellNodes", () => {
+    expect(firstCellRowTwo).toBeInstanceOf(TableCellNode)
+    expect(secondCellRowTwo).toBeInstanceOf(TableCellNode)
+    expect(firstCellRowOne).toBeInstanceOf(TableCellNode)
+    expect(secondCellRowOne).toBeInstanceOf(TableCellNode)
+  })
+  test("appends header cells to the first row, and body cells to subsequent rows", () => {
+    expect(firstCellRowOneHeaderState).toEqual(TableCellHeaderStates.ROW)
+    expect(secondCellRowOneHeaderState).toEqual(TableCellHeaderStates.ROW)
+
+    expect(firstCellRowTwoHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
+    expect(secondCellRowTwoHeaderState).toEqual(TableCellHeaderStates.NO_STATUS)
   })
 })
 
@@ -207,12 +176,27 @@ describe("createWidthTable", () => {
   const rowCount = Math.floor(Math.random() * 10)
   const columnCount = Math.floor(Math.random() * 10)
   const width = 500
+  let tableNode: CustomTableNode
+  let numberOfRows: number
+  let numberOfColumns: number
 
-  it("returns a tableNode", () => {
-    let tableNode
-    testEditor.update(() => {
-      tableNode = $createWidthTable(rowCount, columnCount, width)
-    })
+  testEditor.update(() => {
+    tableNode = $createWidthTable(rowCount, columnCount, width)
+    const rowNode = tableNode.getFirstChild() as TableRowNode
+    numberOfRows = tableNode.getChildrenSize()
+    numberOfColumns = rowNode.getChildrenSize()
+  })
+
+  test("returns a tableNode", () => {
     expect(tableNode).toBeInstanceOf(CustomTableNode)
+  })
+  test("the tableNode has a width property of 500", () => {
+    expect(tableNode.__width).toEqual(width)
+  })
+  test("the tableNode has the given number of rows", () => {
+    expect(numberOfRows).toEqual(rowCount)
+  })
+  test("the tableNode has the given number of columns", () => {
+    expect(numberOfColumns).toEqual(columnCount)
   })
 })
